8980326919e47de0471d38e377425c88b34b035e
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-11-03 00:28:32 -0300

    Add github PR template

---
+++ .github/pull_request_template.md
@@ -0,0 +1,10 @@
+# Description
+
+ What is this pull request for? Add a little description about what was done and why. Add images or gists if needed.
+
+# Type of PR
+ - [] Bugfix
+ - [] New feature
+
+# Changes made to code
+ What have you added/removed
--- App.tsx
+++ App.tsx
@@ -3,8 +3,18 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Routes from './src/routes';
 
+
 export default function App() {
   return (
     <Routes />
   );
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
